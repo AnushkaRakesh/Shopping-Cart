78404318bd6714d5b1b01d4a0ac9b3f6b02c71a3
AnushkaRakesh <93435221+AnushkaRakesh@users.noreply.github.com>
2022-07-05 11:02:56 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,27 @@
+const list =[
+    {
+        id:1,
+        title:"White Tshirt",
+        author:"Lorem ipsum dolor sit amet consectetur adipiscing elit",
+        price:550,
+        img:"https://cdn.pixabay.com/photo/2017/01/13/04/56/t-shirt-1976334_960_720.png",
+        amount:1,
+    },
+    {
+        id:2,
+        title:"Cotton Shirt",
+        author:"Lorem ipsum dolor sit amet consectetur adipiscing elit",
+        price:250,
+        img:"https://cdn.pixabay.com/photo/2016/11/23/06/57/isolated-t-shirt-1852114_960_720.png",
+        amount:2,
+    },
+    {
+        id:3,
+        title:"Full Sleeve Tshirt",
+        author:"Lorem ipsum dolor sit amet consectetur adipiscing elit",
+        price:200,
+        img:"https://cdn.pixabay.com/photo/2016/11/23/06/57/isolated-t-shirt-1852113_960_720.png",
+        amount:3,
+    },
+];
+export default list;
